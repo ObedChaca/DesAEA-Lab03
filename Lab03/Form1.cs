015f04436d2e49be625223a7f4cef9bb2cba1f40
Obed Chaca <obed.chaca@tecsup.edu.pe>
2021-09-04 01:14:02 -0500

    Agregar archivos de proyecto.

---
+++ Lab03/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab03
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            String servidor = txtServidor.Text;
+            String bd = txtBaseDatos.Text;
+            String user = txtUsuario.Text;
+            String pwd = txtPassword.Text;
+
+            String str = "Server=" + servidor + ";DataBase=" + bd + ";";
+
+            if (chkAutenticacion.Checked)
+                str += "Integrated Security=true";
+            else str += "User Id=" + user + ";Password=" + pwd + ";";
+
+            try
+            {
+                conn = new SqlConnection(str);
+                conn.Open();
+                MessageBox.Show("Conectado satisfactoriamente");
+                btnDesconectar.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar el servidor: \n" + ex.ToString());
+            }
+        }
+
+        private void chkAutenticacion_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkAutenticacion.Checked)
+            {
+                txtUsuario.Enabled = false;
+                txtPassword.Enabled = false;
+            }
+            else
+            {
+                txtUsuario.Enabled = true;
+                txtPassword.Enabled = true;
+            }
+        }
+
+        private void btnEstado_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Open)
+                    MessageBox.Show("El estado del servidor: " + conn.State + "\n Version del servidor: " + conn.ServerVersion + "\nBase de datos: " + conn.Database);
+                else
+                    MessageBox.Show("Estado del servidor :" + conn.State);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Imposible determinar el estado del servidor: \n" + ex.ToString());
+            }
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State != ConnectionState.Closed)
+                {
+                    MessageBox.Show("Conexion cerrada satisfactoriamente");
+                }
+                else
+                    MessageBox.Show("La conexión ya esta cerrada");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al cerrar la conexión: \n" + ex.ToString());
+            }
+        }
+
+        private void btnPersona_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(conn);
+            form2.Show();
+        }
+    }
+}
---
+++ Lab03/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab03
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection conn;
+        public Form2(SqlConnection conn)
+        {
+            this.conn = conn;
+            InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    List<Person> people = new List<Person>();
+                    SqlCommand command = new SqlCommand("USP_GetPerson", conn);
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlParameter parameter1 = new SqlParameter();
+                    parameter1.SqlDbType = SqlDbType.VarChar;
+                    parameter1.Size = 50;
+                    parameter1.Value = txtNombre.Text.Trim();
+                    parameter1.ParameterName = "@UsuarioName";
+                    command.Parameters.Add(parameter1);
+                    SqlDataReader dataReader = command.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        people.Add(new Person
+                        {
+                            Id = dataReader[0].ToString(),
+                            Name = dataReader[1].ToString()
+                        });
+                    }
+                    conn.Close();
+                    dgvListado.DataSource = people;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error en el listado: \n" + ex.ToString());
+            }
+        }
+    }
+}
